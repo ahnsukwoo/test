1be615cae2a19408e99cf3d9110126bd3569b5a0
ahnsukwoo <patrick0503@daum.net>
2023-09-16 13:22:17 +0900

    argparse가 적용된 camera

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("hello world")
+import argparse
+
+parser = argparse.ArgumentParser(description='call my name')
+parser.add_argument('name', type=str, help='your name')
+args = parser.parse_args()
+
+name = args.name
+
+
+print(f'Hello, {name}!')
